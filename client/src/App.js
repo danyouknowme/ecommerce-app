846130e39501a2b22af05a0a0d9a2b764e98802e
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-10-10 01:58:31 +0700

    responsive navbar for mobile size

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,12 @@ import Cart from "./pages/Cart";
 const App = () => {
   return (
     <div className="app">
-      {/* <Home /> */}
+      <Home />
       {/* <ProductList /> */}
       {/* <Product /> */}
       {/* <Register /> */}
       {/* <Login /> */}
-      <Cart />
+      {/* <Cart /> */}
     </div>
   )
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,9 +2,11 @@ import React from "react";
 import styled from "styled-components";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import Badge from "@material-ui/core/Badge";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 60px;
+  ${mobile({ height: "50px" })};
 `;
 
 const Wrapper = styled.div`
@@ -12,6 +14,7 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  ${mobile({ padding: "10px 0" })};
 `;
 
 const Left = styled.div`
@@ -23,6 +26,7 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 16px;
   cursor: pointer;
+  ${mobile({ display: "none" })};
 `;
 
 const SearchContainer = styled.div`
@@ -35,6 +39,7 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
   border: none;
+  ${mobile({ width: "50px" })};
 
   &:focus {
     outline: none;
@@ -48,6 +53,7 @@ const Center = styled.div`
 const Logo = styled.h1`
   font-weight: bold;
   text-align: center;
+  ${mobile({ fontSize: "24px" })};
 `;
 
 const Right = styled.div`
@@ -55,12 +61,14 @@ const Right = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: "center" })};
 `;
 
 const MenuItem = styled.div`
   font-size: 16px;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })};
 `;
 
 const Navbar = () => {
@@ -70,7 +78,7 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input></Input>
+            <Input placeholder="Search" />
             <Search style={{color: "gray", fontSize: "16px", cursor: "pointer"}}/>
           </SearchContainer>
         </Left>
