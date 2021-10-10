c132e64ce834d321d1631debce11061f6457daa5
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-10-10 19:38:24 +0700

    connect database to mongodb connection

--- api/server.js
+++ api/server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
-app.listen(8080, () => {
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database connection is successful!"))
+  .catch((err) => console.log(err));
+
+app.listen(8080 || process.env.PORT, () => {
   console.log("Backend server is started at port 8080.");
 });
