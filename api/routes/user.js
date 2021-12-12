822190ff3f8352c5a3b49ec07911ba9d8a570e9a
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-12-12 23:55:22 +0700

    setup all routes

---
+++ api/routes/user.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const { verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
+
+// Update
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString()
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+      $set: req.body
+    }, { new: true });
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id)
+    res.status(200).json("User has been deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get All User
+router.delete("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
